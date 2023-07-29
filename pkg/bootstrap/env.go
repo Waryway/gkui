80ffa04b9a5fec1e830c2cec18236a4f62f3957b
KyleWiering <kyle@waryway.com>
2023-07-29 08:47:26 -0500

    #6 Adding a configuration loader pkg with envvar overrides.

---
+++ pkg/bootstrap/env.go
@@ -0,0 +1,136 @@
+package bootstrap
+
+import (
+	"errors"
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+const (
+	YML = "yaml"
+	DEF = "default"
+	REQ = "required"
+)
+
+type BootStrap interface {
+}
+
+type Config[boot BootStrap] struct {
+	BootStrap boot
+	BootCh    chan Config[boot]
+	ErrCh     chan error
+	file      *[]byte
+	location  string
+}
+
+// Init will read the settings.yml file into the bootstrap config.
+// It will override and Environment Variable settings.
+// Also initializes the channels
+func (c *Config[boot]) Init() *Config[boot] {
+	c.location = "settings.yaml"
+	c.BootCh = make(chan Config[boot])
+	c.ErrCh = make(chan error)
+
+	return c
+}
+
+// Load will persist the current applications bootstrap settings to the settings.yml file.
+func (c *Config[boot]) Load() *Config[boot] {
+	go func(conf *Config[boot]) {
+		c.ReadSettingFile().LoadYaml().LoadEnv()
+		c.BootCh <- *c
+	}(c)
+
+	select {
+	case err := <-c.ErrCh:
+		fmt.Println(err)
+	case res := <-c.BootCh:
+		return &res
+	}
+
+	return c
+}
+
+// Save will persist the current applications bootstrap settings to the settings.yml file.
+func (c *Config[boot]) Save() {
+
+}
+
+// LoadEnv checks for any configured BootStrap values from the environment.
+// Effectively can only override top level yaml configuration settings.
+// Adapted from https://github.com/Vish511/GoENV/blob/master/envconfigloader.go
+func (c *Config[boot]) LoadEnv() *Config[boot] {
+	v := reflect.ValueOf(c.BootStrap)
+	var failed []string
+	failedCount := 0
+
+	for i := 0; i < v.NumField(); i++ {
+		tagName := v.Type().Field(i).Tag.Get(YML)
+		tagDefault := v.Type().Field(i).Tag.Get(DEF)
+
+		if tagName == "" || tagName == "-" {
+			continue
+		}
+
+		envVal, Defaulted := loadFromEnv(tagName, tagDefault, c.ErrCh)
+
+		if Defaulted {
+			log.Println("Loaded: " + tagName + "=" + envVal + " with default")
+		} else {
+			log.Println("Loaded: " + tagName + "=" + envVal)
+		}
+
+		if on, err := strconv.ParseBool(v.Type().Field(i).Tag.Get(REQ)); err == nil && on && envVal == "" {
+			failedCount++
+			failed = append(failed, tagName)
+			log.Println("Missing: " + tagName + "=" + "")
+		}
+
+		reflect.ValueOf(&c.BootStrap).Elem().Field(i).SetString(envVal)
+
+		if failedCount > 0 {
+			c.ErrCh <- errors.New("Failed to load" + strconv.Itoa(failedCount) + " vars")
+		}
+	}
+
+	return c
+}
+
+func (c *Config[boot]) LoadYaml() *Config[boot] {
+	if err := yaml.Unmarshal(*c.file, &c.BootStrap); err != nil {
+		c.ErrCh <- err
+	}
+
+	return c
+}
+
+func (c *Config[boot]) ReadSettingFile() *Config[boot] {
+	if file, err := os.ReadFile(c.location); err != nil {
+		log.Println("Failed to read" + c.location)
+		c.ErrCh <- err
+	} else {
+		c.file = &file
+	}
+
+	return c
+}
+
+// loadFromEnv Given a key lookup the envvar with a default fallback. return the value and true if the default was used.
+// k is the envvar key.
+// def is the default value to use if the envvar is empty.
+func loadFromEnv(k string, def string, errorCh chan error) (string, bool) {
+	if v := os.Getenv(k); v == "" {
+		if err := os.Setenv(k, def); err != nil {
+			errorCh <- err
+		} else {
+			v = def
+		}
+		return v, true
+	} else {
+		return v, false
+	}
+}
---
+++ pkg/bootstrap/env_test.go
@@ -0,0 +1,167 @@
+package bootstrap
+
+import (
+	"testing"
+)
+
+type exampleSettings struct {
+	A string `yaml:"a"`
+}
+
+//func TestConfig_Init(t *testing.T) {
+//	type testCase[boot BootStrap] struct {
+//		name string
+//		c    Config[boot]
+//		want *Config[boot]
+//	}
+//	tests := []testCase[exampleSettings]{
+//		{
+//			name: "some test",
+//			c: ,
+//			want:,
+//		},
+//	},
+//		for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got := tt.c.Init(); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("Init() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+
+//
+//func TestConfig_Load(t *testing.T) {
+//	type testCase[boot BootStrap] struct {
+//		name string
+//		c    Config[boot]
+//	}
+//	tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			tt.c.Load()
+//		})
+//	}
+//}
+//
+//func TestConfig_LoadEnv(t *testing.T) {
+//	type testCase[boot BootStrap] struct {
+//		name string
+//		c    Config[boot]
+//		want *Config[boot]
+//	}
+//	tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got := tt.c.LoadEnv(); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("LoadEnv() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+
+func TestConfig_LoadYaml(t *testing.T) {
+	bootstrap := Config[exampleSettings]{BootStrap: exampleSettings{}}
+	expected := Config[exampleSettings]{BootStrap: exampleSettings{A: "thing"}}
+	bootstrap.Init()
+	expected.Init()
+
+	data := []byte(`
+a: thing
+b: a string from struct B`)
+	bootstrap.file = &data
+	type testCase[boot BootStrap] struct {
+		name string
+		c    Config[boot]
+		want *Config[boot]
+	}
+
+	tests := []testCase[exampleSettings]{
+		{
+			name: "some test",
+			c:    bootstrap,
+			want: &expected,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			go func() {
+				tt.c.BootCh <- *tt.c.LoadYaml()
+			}()
+
+			select {
+			case err := <-tt.c.ErrCh:
+				t.Error(err)
+			case got := <-tt.c.BootCh:
+				if got.BootStrap.A != "thing" {
+					t.Errorf("LoadYaml() = %v, want %v", got.BootStrap.A, tt.want.BootStrap.A)
+				}
+			}
+		})
+	}
+}
+
+//
+//func TestConfig_ReadSettingFile(t *testing.T) {
+//	type testCase[boot BootStrap] struct {
+//		name string
+//		c    Config[boot]
+//		want *Config[boot]
+//	}
+//	tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			if got := tt.c.ReadSettingFile(); !reflect.DeepEqual(got, tt.want) {
+//				t.Errorf("ReadSettingFile() = %v, want %v", got, tt.want)
+//			}
+//		})
+//	}
+//}
+//
+//func TestConfig_Save(t *testing.T) {
+//	type testCase[boot BootStrap] struct {
+//		name string
+//		c    Config[boot]
+//	}
+//	tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+//		// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			tt.c.Save()
+//		})
+//	}
+//}
+
+func Test_loadFromEnv(t *testing.T) {
+	type args struct {
+		k       string
+		def     string
+		errorCh chan error
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := loadFromEnv(tt.args.k, tt.args.def, tt.args.errorCh)
+			if got != tt.want {
+				t.Errorf("loadFromEnv() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("loadFromEnv() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
